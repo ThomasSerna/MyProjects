80d9f5ea59bc27a26b5ffc4e5a3cda4adf5536ad
xThoms <88681523+xThoms@users.noreply.github.com>
2022-05-11 17:50:37 -0500

    Update and rename Radio de un circulo.py to Circunferencia de un circulo.py

--- Circunferencia de un circulo.py
+++ Circunferencia de un circulo.py
@@ -1,5 +1,5 @@
 #           Notas
-#Este proyecto esta echo de la finalidad de obtener el radio de un circulo, a traves de su diametro
+#Este proyecto esta echo de la finalidad de obtener la circunferencia de un circulo, a traves de su diametro
 
 Diamentro = float(input("Dame el diamentro de tu circulo: "))
 Pi = 3.14159265359
